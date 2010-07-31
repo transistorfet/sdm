627926a3d227360b440a1f45f211fc068fbb23ec
transistor <trans@lorax.jabberwocky.ca>
2010-07-31 13:03:14 -0700

    Moved the template definitions to the header files as required. =(

--- include/sdm/array.h
+++ include/sdm/array.h
@@ -6,6 +6,9 @@
 #ifndef _SDM_ARRAY_H
 #define _SDM_ARRAY_H
 
+#include <sdm/memory.h>
+#include <sdm/globals.h>
+
 #define MOO_ABF_DELETE			0x01	// Delete elements when removed from list
 #define MOO_ABF_DELETEALL		0x02	// Delete all elements when list is destroyed
 #define MOO_ABF_RESIZE			0x04	// Allow resizing of the list when needed
@@ -21,7 +24,7 @@ class MooArray {
 	int m_size;
 	int m_max;
 	int m_next_space;
-	T **m_data;
+	T *m_data;
 
     public:
 	MooArray(int size = MOO_ARRAY_DEFAULT_SIZE, int max = -1, int bits = MOO_ARRAY_DEFAULT_BITS);
@@ -37,6 +40,126 @@ class MooArray {
 
 	void resize(int size);
 };
+
+
+template<typename T>
+MooArray<T>::MooArray(int size, int max, int bits)
+{
+	m_bits = bits;
+	m_size = 0;
+	m_max = max;
+	m_next_space = 0;
+	m_data = NULL;
+	this->resize(size);
+}
+
+template<typename T>
+MooArray<T>::~MooArray()
+{
+	int i;
+
+	if (m_bits & MOO_ABF_DELETEALL) {
+		for (i = 0; i < m_size; i++) {
+			if (m_data[i])
+				delete m_data[i];
+		}
+	}
+	memory_free(m_data);
+}
+
+template<typename T>
+T MooArray<T>::operator[](int index)
+{
+	return(this->get(index));
+}
+
+template<typename T>
+T MooArray<T>::get(int index)
+{
+	if (index > m_size)
+		return(NULL);
+	return(m_data[index]);
+}
+
+template<typename T>
+T MooArray<T>::set(int index, T value)
+{
+	if (index > m_size) {
+		if ((m_bits & MOO_ABF_RESIZE) && (m_max == -1 || index < m_max))
+			this->resize(index + 1);
+		else
+			return(NULL);
+	}
+
+	if (m_data[index]) {
+		if (!(m_bits & MOO_ABF_REPLACE))
+			return(NULL);
+		if (m_bits & MOO_ABF_DELETE)
+			delete m_data[index];
+	}
+	m_data[index] = value;
+
+	/** Update the next_space value accordingly */
+	if (!value) {
+		if (index < m_next_space)
+			m_next_space = index;
+	}
+	else {
+		for (; m_next_space < m_size; m_next_space++) {
+			if (!m_data[m_next_space])
+				break;
+		}
+	}
+	return(value);
+}
+
+template<typename T>
+int MooArray<T>::add(T value)
+{
+	int space = m_next_space;
+
+	if (space >= m_size && m_bits & MOO_ABF_RESIZE)
+		this->resize((int) (m_size * MOO_ARRAY_GROWTH_FACTOR));
+	if (this->set(space, value))
+		return(space);
+	return(-1);
+}
+
+template<typename T>
+int MooArray<T>::remove(T value)
+{
+	int i;
+
+	for (i = 0; i < m_size; i++) {
+		if (m_data[i] == value) {
+			if (m_bits & MOO_ABF_DELETE)
+				delete m_data[i];
+			m_data[i] = NULL;
+			if (i < m_next_space)
+				m_next_space = i;
+			return(1);
+		}
+	}
+	return(0);
+}
+
+template<typename T>
+void MooArray<T>::resize(int size)
+{
+	T *newdata;
+
+	if (m_max > 0 && size > m_max)
+		size = m_max;
+	if (size == m_size)
+		throw -1;
+
+	if (!(newdata = (T *) memory_realloc(m_data, size * sizeof(T))))
+		throw -1;
+	m_data = newdata;
+	memset(&m_data[m_size], '\0', (size - m_size) * sizeof(T));
+	m_next_space = m_size;
+	m_size = size;
+}
 
 #endif
 
--- include/sdm/hash.h
+++ include/sdm/hash.h
@@ -7,6 +7,7 @@
 #ifndef _SDM_HASH_H
 #define _SDM_HASH_H
 
+#include <sdm/memory.h>
 #include <sdm/globals.h>
 
 #define MOO_HBF_NO_ADD			0x0001		// No entries can be added
@@ -17,6 +18,8 @@
 
 #define MOO_HASH_DEFAULT_SIZE		32
 #define MOO_HASH_DEFAULT_BITS		MOO_HBF_REPLACE | MOO_HBF_REMOVE | MOO_HBF_DELETEALL
+#define MOO_HASH_LOAD_FACTOR		0.75
+#define MOO_HASH_GROWTH_FACTOR		1.75
 
 template<typename T>
 class MooHashEntry {
@@ -52,6 +55,214 @@ class MooHash {
 	T next();
 };
 
+
+#define LOWERCASE(ch) \
+	( (ch >= 0x41 && ch <= 0x5a) ? ch + 0x20 : ch )
+
+static inline unsigned int moo_hash_func(const char *);
+
+template<typename T>
+MooHash<T>::MooHash(int size, int bits)
+{
+	if (size <= 0)
+		size = MOO_HASH_DEFAULT_SIZE;
+	m_bits = bits;
+	m_traverse_index = 0;
+	m_traverse_next = NULL;
+	m_size = size;
+	m_entries = 0;
+	if (!(m_table = (MooHashEntry<T> **) memory_alloc(m_size * sizeof(MooHashEntry<T> *))))
+		throw(-1);
+	memset(m_table, '\0', m_size * sizeof(MooHashEntry<T> *));
+}
+
+template<typename T>
+MooHash<T>::~MooHash()
+{
+	int i;
+	MooHashEntry<T> *cur, *next;
+
+	for (i = 0;i < m_size;i++) {
+		cur = m_table[i];
+		while (cur) {
+			next = cur->m_next;
+			if (m_bits & MOO_HBF_DELETEALL)
+				delete cur->m_data;
+			delete cur;
+			cur = next;
+		}
+	}
+	memory_free(m_table);
+}
+
+template<typename T>
+int MooHash<T>::set(const char *key, T data)
+{
+	unsigned int hash;
+	MooHashEntry<T> *entry;
+
+	if (!key || !data || (m_bits & MOO_HBF_NO_ADD))
+		return(-1);
+	/** Search for an existing entry */
+	hash = moo_hash_func(key) % m_size;
+	for (entry = m_table[hash]; entry; entry = entry->m_next) {
+		if (!strcasecmp(key, entry->m_key)) {
+			if (!(m_bits & MOO_HBF_REPLACE))
+				return(-1);
+			if (m_bits & MOO_HBF_DELETE)
+				delete entry->m_data;
+			entry->m_data = data;
+			return(0);
+		}
+	}
+
+	if (!(entry =  (MooHashEntry<T> *) memory_alloc(sizeof(MooHashEntry<T>) + strlen(key) + 1)))
+		return(-1);
+	entry->m_key = (char *) (entry + 1);
+	strcpy(entry->m_key, key);
+	entry->m_data = data;
+
+	entry->m_next = m_table[hash];
+	m_table[hash] = entry;
+	m_entries++;
+	if ((m_entries / m_size) > MOO_HASH_LOAD_FACTOR)
+		this->rehash((int) (m_size * MOO_HASH_GROWTH_FACTOR));
+	return(0);
+}
+
+template<typename T>
+int MooHash<T>::remove(const char *key)
+{
+	unsigned int hash;
+	MooHashEntry<T> *cur, *prev;
+
+	if (!key || !(m_bits & MOO_HBF_REMOVE))
+		return(-1);
+	hash = moo_hash_func(key) % m_size;
+	prev = NULL;
+	cur = m_table[hash];
+	while (cur) {
+		if (!strcasecmp(key, cur->m_key)) {
+			// TODO this could cause a bug where an entry is skipped
+			if (m_traverse_next == cur)
+				this->next_entry();
+			if (prev)
+				prev = cur->m_next;
+			else
+				m_table[hash] = cur->m_next;
+			if (m_bits & MOO_HBF_DELETE)
+				delete cur->m_data;
+			memory_free(cur);
+			m_entries--;
+			return(0);
+		}
+		prev = cur;
+		cur = cur->m_next;
+	}
+	return(-1);
+}
+
+template<typename T>
+MooHashEntry<T> *MooHash<T>::get_entry(const char *key)
+{
+	unsigned int hash;
+	MooHashEntry<T> *cur;
+
+	hash = moo_hash_func(key) % m_size;
+	for (cur = m_table[hash % m_size]; cur; cur = cur->m_next) {
+		if (!strcasecmp(key, cur->m_key))
+			return(cur);
+	}
+	return(NULL);
+}
+
+template<typename T>
+T MooHash<T>::get(const char *key)
+{
+	unsigned int hash;
+	MooHashEntry<T> *cur;
+
+	hash = moo_hash_func(key) % m_size;
+	for (cur = m_table[hash % m_size]; cur; cur = cur->m_next) {
+		if (!strcasecmp(key, cur->m_key))
+			return(cur->m_data);
+	}
+	return(NULL);
+}
+
+template<typename T>
+void MooHash<T>::reset()
+{
+	for (m_traverse_index = 0; m_traverse_index < m_size; m_traverse_index++) {
+		if ((m_traverse_next = m_table[m_traverse_index]))
+			return;
+	}
+}
+
+template<typename T>
+MooHashEntry<T> *MooHash<T>::next_entry()
+{
+	MooHashEntry<T> *entry;
+
+	if ((m_traverse_index >= m_size) || !(entry = m_traverse_next))
+		return(NULL);
+	else if ((m_traverse_next = m_traverse_next->m_next))
+		return(entry);
+
+	while (++m_traverse_index < m_size) {
+		if ((m_traverse_next = m_table[m_traverse_index]))
+			return(entry);
+	}
+	return(entry);
+}
+
+template<typename T>
+T MooHash<T>::next()
+{
+	MooHashEntry<T> *entry;
+
+	entry = this->next_entry();
+	if (!entry)
+		return(NULL);
+	return(entry->m_data);
+}
+
+template<typename T>
+int MooHash<T>::rehash(int newsize)
+{
+	unsigned int i, hash, oldsize;
+	MooHashEntry<T> **newtable;
+	MooHashEntry<T> *cur, *next;
+
+	if (!(newtable = (MooHashEntry<T> **) memory_alloc(newsize * sizeof(MooHashEntry<T> *))))
+		return(-1);
+	memset(newtable, '\0', newsize * sizeof(MooHashEntry<T> *));
+	oldsize = m_size;
+	m_size = newsize;
+	for (i = 0;i < oldsize;i++) {
+		cur = m_table[i];
+		while (cur) {
+			next = cur->m_next;
+			hash = moo_hash_func(cur->m_key) % m_size;
+			cur->m_next = newtable[hash];
+			newtable[hash] = cur;
+			cur = next;
+		}
+	}
+	memory_free(m_table);
+	m_table = newtable;
+	return(0);
+}
+
+static inline unsigned int moo_hash_func(const char *str)
+{
+	int i;
+	unsigned int hash = 0;
+
+	for (i = 0; str[i] != '\0'; i++)
+		hash = LOWERCASE(str[i]) + (hash << 6) + (hash << 16) - hash;
+	return(hash);
+}
 
 #endif
 
